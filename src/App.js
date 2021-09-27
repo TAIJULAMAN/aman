7c3710986292f3d2ca11277884c4b6a7fc38eef8
TAIJULAMAN <taijulaman8@gmail.com>
2021-09-27 21:54:25 +0600

    hello

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
